6e095ec10ae2967debe72692a3e5d525bd6b1071
mshipper <mshipper@yahoo.com>
2026-01-09 05:53:51 +0000

    Work in progres!

--- internal/slicetree/SliceTree.go
+++ internal/slicetree/SliceTree.go
@@ -1,40 +1,237 @@
 package slicetree
 
+import (
+	"fmt"
+	"slices"
+)
+
 type SliceTree[T any] struct {
+
+	// Internally managed slice
 	Slice []T
-	Cmp   func(a, b T) int
+
+	// Compare function.
+	Cmp func(a, b T) int
+
+	//  Tells the internals how far to go with indexing, the internals will scan any remaining chunk less than this value.
+	// If set to any number less than 2, scanning is disabled and we will use btree style indexing for everything.
+	// The recommended value is 3.
+	ScanAt int
+
+	// Required non 0 value, determins by what capacity we grow the internal
+	// Slice.  Default is 1.
+	Grow int
+
+	// Required non nil value, called when ever a value is overwritten
+	OnOverrite func(oldValue, newValue T)
 }
 
+func LookString(nextBegin, nextEnd, nextMid, offset int, resolved bool) string {
+	return fmt.Sprintf("nextBegin: %d, nextEnd: %d, nextMid: %d, offset: %d, resolved: %v", nextBegin, nextEnd, nextMid, offset, resolved)
+}
+
+func StubOnOverwrite[T any](old, new T) {}
 func NewSliceTree[T any](size int, cb func(a, b T) int) *SliceTree[T] {
 	return &SliceTree[T]{
-		Slice: make([]T, 0, size),
-		Cmp:   cb,
+		Slice:      make([]T, 0, size),
+		Cmp:        cb,
+		ScanAt:     3,
+		Grow:       1,
+		OnOverrite: StubOnOverwrite[T],
 	}
 }
 
 func (s *SliceTree[T]) GetMid(size int) int {
-	return size/2 + size&1
+	return (size-2)/2 + size&1
 }
 
-func (s *SliceTree[T]) ClearBefore(v T) {
+func (s *SliceTree[T]) ClearBefore(v T) []T {
+	return nil
 }
 
-func (s *SliceTree[T]) ClearAfter(v T) {
+func (s *SliceTree[T]) ClearAfter(v T) []T {
+	return nil
 }
 
-func (s *SliceTree[T]) ClearTo(v T) {
+func (s *SliceTree[T]) ClearTo(v T) []T {
+	return nil
+}
+func (s *SliceTree[T]) ClearFrom(v T) []T {
+	return nil
 }
 
-func (s *SliceTree[T]) GetIndex(v T) (idx int) {
+func (s *SliceTree[T]) Remove(v T) bool {
+	return false
+}
+
+func (s *SliceTree[T]) Add(v T) (index int) {
+	total := cap(s.Slice)
+
+	if total == 0 {
+		// 0 size.. just append
+		s.Slice = append(s.Slice, v)
+		return 0
+	}
+	idx, offset := s.GetIndex(v)
+	return s.setIdx(idx, offset, v)
+}
+
+func (s *SliceTree[T]) setIdx(idx, offset int, v T) (index int) {
+	size := len(s.Slice)
+	if offset != 0 {
+		ns := size + 1
+		s.grow(ns)
+		s.Slice = append(s.Slice, v)
+		switch idx {
+		case size:
+			if offset == 1 {
+				// add new value
+				s.Slice = append(s.Slice, v)
+				return ns
+			} else {
+				// move the old value
+				s.Slice = append(s.Slice, s.Slice[idx])
+				// set the new value
+				s.Slice[idx] = v
+				return idx
+			}
+		case 0:
+			if offset == 1 {
+				copy(s.Slice[2:], s.Slice[1:size])
+				s.Slice[1] = v
+				return 1
+			} else {
+				copy(s.Slice[1:], s.Slice[0:size])
+				s.Slice[0] = v
+			}
+			return 0
+		default:
+			index = idx + offset
+			copy(s.Slice[index+1:], s.Slice[index:size])
+			if offset < 0 {
+				s.Slice[idx] = v
+				return idx
+			} else {
+				s.Slice[index] = v
+				return index
+			}
+
+		}
+	} else {
+		s.OnOverrite(s.Slice[idx], v)
+		s.Slice[idx] = v
+		return idx
+	}
+}
+
+func (s *SliceTree[T]) grow(size int) {
+	if cap(s.Slice) < size {
+
+		s.Slice = slices.Grow(s.Slice, s.Grow)
+	}
+
+}
+
+func (s *SliceTree[T]) GetIndex(v T) (index, offset int) {
 
 	size := len(s.Slice)
 	if size == 0 {
 		// set is empty!
-		return -2
+		return 0, 1
 	}
-	mid := s.GetMid(size)
-	if mid == size {
-		return s.Cmp(v, s.Slice[0])
+	nextMid := s.GetMid(size)
+	if nextMid == -1 {
+		// only one element
+		return 0, s.Cmp(v, s.Slice[0])
+	}
+
+	// well if we get here.. we need to walk the tree
+	nextBegin := 0
+	nextEnd := len(s.Slice) - 1
+	var resolved bool
+
+	for {
+		nextBegin, nextEnd, nextMid, offset, resolved = s.ResolveNext(nextBegin, nextEnd, nextMid, v)
+		if resolved {
+			index = offset + nextMid
+			offset = s.Cmp(v, s.Slice[index])
+			break
+		}
+
+	}
+
+	return
+}
+
+func (s *SliceTree[T]) LookAhead(begin, end, mid int, v T) (nextBegin, nextEnd, nextMid, offset int, resolved bool) {
+	nextBegin = mid + 1
+	diff := end - nextBegin
+
+	if diff <= 0 {
+		resolved = true
+		nextMid = mid
+		offset = 1
+		return
+	} else if diff < s.ScanAt {
+		resolved = true
+		nextMid, offset = s.ScanSet(begin, nextEnd, v)
+		return
+	}
+	nextMid = nextBegin + s.GetMid(diff+1)
+	nextEnd = end
+	offset = s.Cmp(s.Slice[nextMid], v)
+	resolved = offset == 0
+	return
+}
+
+func (s *SliceTree[T]) ScanSet(begin, end int, v T) (offset, index int) {
+	for i := begin; i <= end; i++ {
+
+		offset = s.Cmp(v, s.Slice[i])
+		index = i
+
+		if offset < 1 {
+			// if we got here, then the value is either equal to or before this value
+			return
+		}
+	}
+	return
+}
+
+func (s *SliceTree[T]) LookBehind(begin, end, mid int, v T) (nextBegin, nextEnd, nextMid, offset int, resolved bool) {
+	nextEnd = mid - 1
+	diff := nextEnd - begin
+
+	if diff <= 0 {
+		resolved = true
+		nextMid = mid
+		offset = -1
+		return
+	} else if diff < s.ScanAt {
+		resolved = true
+		nextMid, offset = s.ScanSet(begin, nextEnd, v)
+		return
+	}
+	nextMid = begin + s.GetMid(diff+1)
+	nextBegin = begin
+	offset = s.Cmp(s.Slice[nextMid], v)
+	resolved = offset == 0
+	return
+}
+
+func (s *SliceTree[T]) ResolveNext(begin, end, mid int, v T) (nextBegin, nextEnd, nextMid, offset int, resolved bool) {
+
+	cmp := s.Cmp(v, s.Slice[mid])
+	switch cmp {
+	case 0:
+		nextMid = mid
+		resolved = true
+		offset = cmp
+		return
+	case -1:
+		nextBegin, nextEnd, nextMid, offset, resolved = s.LookBehind(begin, end, mid, v)
+	case 1:
+		nextBegin, nextEnd, nextMid, offset, resolved = s.LookAhead(begin, end, mid, v)
 	}
 
 	return
--- internal/slicetree/SliceTree_test.go
+++ internal/slicetree/SliceTree_test.go
@@ -5,19 +5,198 @@ import (
 	"testing"
 )
 
-func TestEmpty(t *testing.T) {
-	t.Logf("starting")
+func TestMid(t *testing.T) {
 	s := NewSliceTree(1, func(a, b int) int {
 		return cmp.Compare(a, b)
 	})
-
-	if got := s.GetIndex(0); got != -2 {
-		t.Fatalf("Index should be -2,got %d", got)
+	if check := s.GetMid(3); check != 1 {
+		t.Fatalf("Expected: 1, got : %d", check)
 	}
-	t.Logf("Cap is: %d", cap(s.Slice))
+	if check := s.GetMid(2); check != 0 {
+		t.Fatalf("Expected: 0, got : %d", check)
+	}
+	if check := s.GetMid(1); check == -1 {
+		t.Fatalf("Expected: -1, got : %d", check)
+	}
+	if check := s.GetMid(0); check == -2 {
+		t.Fatalf("Expected: -2, got : %d", check)
+	}
+	if check := s.GetMid(10); check != 4 {
+		t.Fatalf("Expected: 4, got : %d", check)
+	}
+	if check := s.GetMid(5); check != 2 {
+		t.Fatalf("Expected: 2, got : %d", check)
+	}
+}
+
+func TestScanAt(t *testing.T) {
+	s := &SliceTree[int]{
+		Cmp:    cmp.Compare[int],
+		ScanAt: 1,
+		Slice:  []int{2, 4, 6, 8, 10},
+	}
+	offset, index := s.ScanSet(0, 4, 5)
+	if offset != -1 {
+		t.Errorf("Expected offset of -1, got %d", offset)
+	}
+	if index != 2 {
+		t.Errorf("Expected index of 2, got %d", index)
+	}
+	offset, index = s.ScanSet(2, 3, 6)
+	if offset != 0 {
+		t.Errorf("Expected offset of 0, got %d", offset)
+	}
+	if index != 2 {
+		t.Errorf("Expected index of 2, got %d", index)
+	}
+}
+
+func TestLookBehind(t *testing.T) {
+	s := &SliceTree[int]{
+		Cmp: cmp.Compare[int],
+		// never Scan!
+		ScanAt: -1,
+		//           0  1  2  3  4   5   6
+		Slice: []int{2, 4, 6, 8, 10, 12, 14},
+	}
+	t.Log("In block 1")
+	nextBegin, nextEnd, nextMid, offset, resolved := s.LookBehind(0, 6, 3, 5)
+	t.Log(LookString(nextBegin, nextEnd, nextMid, offset, resolved))
+
+	if resolved {
+		t.Error("should not be resolved")
+	}
+	if nextBegin != 0 {
+		t.Errorf("Expected next begin: 0, got: %d", nextBegin)
+	}
+	if nextEnd != 2 {
+		t.Errorf("Expected next end: 1, got: %d", nextEnd)
+	}
+	if offset != -1 {
+		t.Errorf("Expected offset: -1, got: %d", offset)
+	}
+	if nextMid != 1 {
+		t.Errorf("Expected mid: 1, got: %d", nextMid)
+	}
+	t.Log("In block 2")
+	nextBegin, nextEnd, nextMid, offset, resolved = s.LookBehind(2, 2, 2, 5)
+	t.Log(LookString(nextBegin, nextEnd, nextMid, offset, resolved))
+	if !resolved {
+		t.Fatalf("should be resolved")
+	}
+	if nextMid != 2 {
+		t.Errorf("Expected next mid: 2, got: %d", nextMid)
+	}
+
+}
+
+func TestLookAhead(t *testing.T) {
+	s := &SliceTree[int]{
+		Cmp: cmp.Compare[int],
+		// never Scan!
+		ScanAt: -1,
+		//           0  1  2  3  4   5   6
+		Slice: []int{2, 4, 6, 8, 10, 12, 14},
+	}
+	t.Log("In block 1")
+	nextBegin, nextEnd, nextMid, offset, resolved := s.LookAhead(0, 6, 3, 9)
+	t.Log(LookString(nextBegin, nextEnd, nextMid, offset, resolved))
+
+	if resolved {
+		t.Error("should not be resolved")
+	}
+	if nextBegin != 4 {
+		t.Errorf("Expected next begin: 4, got: %d", nextBegin)
+	}
+	if nextEnd != 6 {
+		t.Errorf("Expected next end: 6, got: %d", nextEnd)
+	}
+	if offset != 1 {
+		t.Errorf("Expected offset: 1, got: %d", offset)
+	}
+	if nextMid != 5 {
+		t.Errorf("Expected mid: 5, got: %d", nextMid)
+	}
+	t.Log("In block 2")
+	nextBegin, nextEnd, nextMid, offset, resolved = s.LookAhead(4, 6, 5, 9)
+	t.Log(LookString(nextBegin, nextEnd, nextMid, offset, resolved))
+	if !resolved {
+		t.Fatalf("should be resolved")
+	}
+	if nextMid != 5 {
+		t.Errorf("Expected next mid: 5, got: %d", nextMid)
+	}
+
+}
+
+func TestGetIn(t *testing.T) {
+
+	s := &SliceTree[int]{
+		Cmp: cmp.Compare[int],
+		// never Scan!
+		ScanAt: -1,
+		//           0  1  2  3  4   5   6
+		Slice: []int{2, 4, 6, 8, 10, 12, 14},
+	}
+	index, offset := s.GetIndex(12)
+	t.Logf("Got index: %d, offset: %d", index, offset)
+	if s.Slice[index+offset] != 12 {
+		t.Fatalf("Failed to fetchg our indexed value")
+	}
+	index, offset = s.GetIndex(2)
+	t.Logf("Got index: %d, offset: %d", index, offset)
+	if s.Slice[index+offset] != 2 {
+		t.Fatalf("Failed to fetchg our indexed value")
+	}
+
+	index, offset = s.GetIndex(14)
+	t.Logf("Got index: %d, offset: %d", index, offset)
+	if s.Slice[index+offset] != 14 {
+		t.Fatalf("Failed to fetchg our indexed value")
+	}
+
+	index, offset = s.GetIndex(15)
+	t.Logf("Got index: %d, offset: %d", index, offset)
+	index, offset = s.GetIndex(1)
+	t.Logf("Got index: %d, offset: %d", index, offset)
+}
+
+func TestIdxSet(t *testing.T) {
+	s := NewSliceTree[int](0, cmp.Compare)
+
 	s.Slice = append(s.Slice, 1)
-	if got := s.GetIndex(0); got != -1 {
-		t.Fatalf("Index should be -1,got %d", got)
-	}
 
+	expected := []int{1, 2}
+	s.setIdx(0, 1, 2)
+	checkExpected(t, "Set 1", expected, s.Slice)
+
+	expected = []int{0, 1, 2}
+	s.setIdx(0, -1, 0)
+	checkExpected(t, "Set 2", expected, s.Slice)
+
+	expected = []int{0, 1, 2, 4}
+	s.setIdx(2, 1, 4)
+	checkExpected(t, "Set 3", expected, s.Slice)
+
+	expected = []int{0, 1, 2, 3, 4}
+	s.setIdx(2, 1, 3)
+	checkExpected(t, "Set 4", expected, s.Slice)
+	//                       3
+	s.Slice = []int{0, 1, 2, 4}
+	s.setIdx(3, -1, 3)
+	checkExpected(t, "Set 5", expected, s.Slice)
+
+}
+
+func checkExpected(t *testing.T, set string, exp, got []int) {
+	t.Logf("** Starting set: %s", set)
+	if len(got) != len(exp) {
+		t.Fatalf("Expected: length of: %d, got length of %d", len(exp), len(got))
+	}
+	for i, check := range exp {
+		t.Logf("id: %d, expected: %d, got %d", i, check, got[i])
+		if got[i] != check {
+			t.Errorf("Missmatch id: %d, expected: %d, got %d", i, check, got[i])
+		}
+	}
 }
